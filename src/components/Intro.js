562e6f596b0d4b394e0f184d52688b97e347aa72
JillKlatt <klatt.jill@gmail.com>
2021-07-15 13:10:04 -0400

    Add basic components

---
+++ src/components/Intro.js
@@ -0,0 +1,15 @@
+
+
+function Intro() {
+
+    return (
+        <div>
+        <h2>Jill Klatt</h2>
+        <h3>Full Stack Software Engineer</h3>
+        </div>
+    )
+
+
+} 
+
+export default Intro
