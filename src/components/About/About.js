1c9def445ad08e9d9e17f25d83bf7e8f9fb59c65
JillKlatt <klatt.jill@gmail.com>
2021-07-16 21:20:35 -0400

    Update about bio

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,12 +2,39 @@ import './About.css'
 
 function About() {
 
-    return(
+    return (
         <div id='about'>
-            <h3>About Me</h3>
-            <p>Here's a picture and a list of languages I know:</p>
-        </div>
-    )
+            <div className='title'>
+                <h3><span style={{ color: '#b07156' }}>&gt;</span> ABOUT ME</h3>
+            </div>
+            <div className='content'>
+                <div id='about-text'>
+                    <p>
+                        In July 2021 I graduated Flatiron School's Software Enginnering Bootcamp. I've learned how to compentently work with
+                    </p>
+                    <ul>
+                        <li>JavaScript (ES6+)</li>
+                        <li>React</li>
+                        <li>Ruby on Rails</li>
+                        <li>Sinatra</li>
+                        <li>SQL / PostgreSQL</li>
+                        <li>HTML / CSS</li>
+                        <li>Git / Github</li>
+
+                    </ul>
+                    <p>Software Engineering gives me such an incredible gateway for creativity and efficiency. I've always strived to make logical and fun solutions to problems, and now I feel that I have the tools to do so in a functional and tangible way.
+                        When I'm not coding, I enjoy spending time consuming and creating Dungeons and Dragons content and hiking with my two old. wonderful dogs.</p>
+                </div>
+                <div id='photo-div' className='prof-pic'>
+                    <img id='prof-pic' src="/profpic.jpg"></img>
+                </div>
+                <div className='pic-border'>
+
+                </div>
+
+                </div>
+            </div>
+            )
 }
 
-export default About
+            export default About
