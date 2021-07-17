af85ce0cf2664672bf990f789ba8f81d6e11e84f
JillKlatt <klatt.jill@gmail.com>
2021-07-17 14:09:23 -0400

    Add custom tab

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -5,9 +5,9 @@ import 'react-tabs/style/react-tabs.css';
 
 const CustomTab = ({ children }) => (
     <Tab>
-      <h1>{children}</h1>
+        <h1>{children}</h1>
     </Tab>
-  );
+);
 
 
 CustomTab.tabsRole = 'Tab';
@@ -26,7 +26,7 @@ function Experience() {
                 <Tabs className='tabs'>
                     <TabList className='tablist'>
                         <Tab id='aldi'><h1>Aldi</h1></Tab>
-                        <Tab id='noasis'><h1>Nature's Oasis</h1></Tab>                      
+                        <Tab id='noasis'><h1>Nature's Oasis</h1></Tab>
                         <Tab id='melt'><h1>Melt</h1></Tab>
                     </TabList>
 
@@ -36,16 +36,19 @@ function Experience() {
                             <li><span style={{ color: '#b07156' }}>&gt;</span> Handled customer relations, complaints, and assistance.</li>
                             <li><span style={{ color: '#b07156' }}>&gt;</span> Oversaw daily operations, hiring and training staff, collaborating with vendors.</li>
                         </ul>
-
                     </TabPanel>
                     <TabPanel>
                         <h2>Kitchen Manager - Lakewood, OH</h2>
+                        <ul className='job-description'>
+                            <li><span style={{ color: '#b07156' }}>&gt;</span> Coordinated and supervised staff according to food safety standards.</li>
+                            <li><span style={{ color: '#b07156' }}>&gt;</span> Developed menu, including food cost analysis.</li>
+                        </ul>
                     </TabPanel>
                     <TabPanel>
                         <h2>Bar Manager - Columbus, OH</h2>
-                        <ul>
-                            <li><span style={{ color: '#b07156' }}>&gt;</span></li>
-                            <li><span style={{ color: '#b07156' }}>&gt;</span></li>
+                        <ul className='job-description'>
+                            <li><span style={{ color: '#b07156' }}>&gt;</span> Controlled the front of house image while ensuring customer satisfaction</li>
+                            <li><span style={{ color: '#b07156' }}>&gt;</span> Managed staff scheduling, training, and organized liquor orders and invoicing.</li>
                         </ul>
                     </TabPanel>
                 </Tabs>
