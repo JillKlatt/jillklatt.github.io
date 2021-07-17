a477ae11d80ffacb35c6e4e5dbbd0cf67b2b8494
JillKlatt <klatt.jill@gmail.com>
2021-07-17 14:08:13 -0400

    Add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import './Footer.css'
+
+
+function Footer() {
+
+    return (
+        <div id='footer'>
+            <p id='jill'>Created by Jill Klatt 2021</p>
+            <p id='brittany'>Inspired by <a href='https://github.com/bchiang7/v4'>Brittany Chiang</a></p>
+        </div>
+    )
+
+
+}
+
+export default Footer
