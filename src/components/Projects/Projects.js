8956a4518f67d983fec931eb61e893aa5cda8cc5
JillKlatt <klatt.jill@gmail.com>
2021-07-16 23:57:52 -0400

    Create project cards and begin organizing

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,15 +1,22 @@
 import './Projects.css'
+import ProjectCard from './ProjectCard'
+
 
 function Projects() {
 
     return (
         <div id='projects'>
             <div className='title'>
-        <h3>Projects</h3>
+            <h3><span style={{ color: '#b07156' }}>&gt;</span> PROJECTS</h3>
             </div>
         <div className='content'>
-            
-        </div>
+            <div className='project-cards'>
+                <ul>
+                    <ProjectCard title="Retail Therapy" path='https://retail-therapy.herokuapp.com' src='/retailtherapy.jpg' alt='Supermarket Aisle' label='Grocery Store Role Play Adventure' />
+                    <ProjectCard title='DM Notebook' path='https://dm-notebook.herokuapp.com/'/>
+                </ul>
+            </div>
+        </div> 
         </div>
     )
 
