cae2ff45153f424100c6d0a5c33837b89b65d46a
JillKlatt <klatt.jill@gmail.com>
2021-07-19 14:40:51 -0400

    Emergency push in case of crash

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -4,21 +4,20 @@ function ProjectCard(props) {
     
     return(
         <div>
-                <div>
-                <figure className='pic-wrap' data-category={props.label}>
-                <a href={props.path} className='project-pic'>
+                <div className='card-item'>
+                <figure class="img__wrap" >
+                <a href={props.path} className='project-link'>
 
-                    <img src={props.src} alt={props.alt}></img>
-                    <div className='hide'>
-                        {props.label}
-                    </div>
+                  
 
-
-                </a>                    
+                <img class="img__img" src={props.src} />
+                <p class="img__description">{props.label}</p>
+                </a>  
+                    <h5>{props.title}</h5>                        
+                    <p>{props.tagline}</p>
+                    <p>{props.description}</p>
                 </figure>
-                <h5>{props.title}</h5>
                 </div>
-
         </div>
     )
 }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,21 +6,24 @@ function Projects() {
 
     return (
         <div id='projects'>
-            <div className='title'>
-            <h3><span style={{ color: '#b07156' }}>&gt;</span> PROJECTS</h3>
+            <div className='project-title'>
+                <h3><span style={{ color: '#b07156' }}>&gt;</span> PROJECTS</h3>
             </div>
-        <div className='project-content'>
-            <div className='project-cards'>
-                
-                    <ProjectCard title="Retail Therapy" path='https://retail-therapy.herokuapp.com' src='/retailtherapy.jpg' alt='Supermarket Aisle' label='Grocery Store Role Play Adventure' />
-                    <ProjectCard title='DM Notebook' path='https://dm-notebook.herokuapp.com/' src='dmnotebook.png' alt="DM Notebook" label='D "ffof" D Beyond Copy with Character, Campaign, and Weapon creation'/>
-                
+            <div className='project-content'>
+                <div className='card-container'>
+
+                    <ProjectCard title="Retail Therapy" path='https://retail-therapy.herokuapp.com' src='/retailtherapy.jpg' alt='Supermarket Aisle' label='React / Redux' tagline='Grocery Store Role Play Adventure' />
+                    <ProjectCard title='DM Notebook' path='https://dm-notebook.herokuapp.com/' src='dmnotebook.png' alt="DM Notebook" label='Ruby on Rails' description='D&D Beyond Copy with Character, Campaign, and Weapon creation' />
+
+                </div>
             </div>
-        </div> 
+
+
+
         </div>
     )
 
 
-} 
+}
 
 export default Projects
