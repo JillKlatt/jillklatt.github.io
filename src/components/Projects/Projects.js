b5556ecb79bf0532acd4ca27afcf21b0b9e2a03c
JillKlatt <klatt.jill@gmail.com>
2021-07-19 17:18:54 -0400

    Add Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,17 +9,13 @@ function Projects() {
             <div className='project-title'>
                 <h3><span style={{ color: '#b07156' }}>&gt;</span> PROJECTS</h3>
             </div>
-            <div className='project-content'>
+        
                 <div className='card-container'>
-
-                    <ProjectCard title="Retail Therapy" path='https://retail-therapy.herokuapp.com' src='/retailtherapy.jpg' alt='Supermarket Aisle' label='React / Redux' tagline='Grocery Store Role Play Adventure' />
+                    <ProjectCard title="Retail Therapy" path='https://retail-therapy.herokuapp.com' src='/retailtherapy.jpg' alt='Supermarket Aisle' label='React / Redux' description='Grocery Store Role Play Adventure' />
                     <ProjectCard title='DM Notebook' path='https://dm-notebook.herokuapp.com/' src='dmnotebook.png' alt="DM Notebook" label='Ruby on Rails' description='D&D Beyond Copy with Character, Campaign, and Weapon creation' />
-
+                    <ProjectCard title="Interpreter Connection" path='https://www.youtube.com/watch?v=wp_YVFNdPhA&ab_channel=JillKlatt' src='/jslogo.png' alt="JS Logo" label='Vanilla JavaScript' description='Educational single page application that displays available interpreters for various cities and languages' />
+                    <ProjectCard title='Character Creator' path='https://www.loom.com/share/440b64a1af284b43a1eefcf6b5d323a4' src='/sinatralogo.png' alt='Sinatra Logo' label='Sinatra' description='Basic Character Creator' />
                 </div>
-            </div>
-
-
-
         </div>
     )
 
