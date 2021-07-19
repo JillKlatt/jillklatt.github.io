a0fc9c5b24246e5db0a3058dbe9f8cf7e065e24f
JillKlatt <klatt.jill@gmail.com>
2021-07-19 17:19:42 -0400

    Remove extra divs

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,24 +1,22 @@
 import './Projects.css'
 
-function ProjectCard(props) {
-    
-    return(
-        <div>
-                <div className='card-item'>
-                <figure class="img__wrap" >
-                <a href={props.path} className='project-link'>
+function ProjectCard({ title, path, src, alt, label, description }) {
 
-                  
-
-                <img class="img__img" src={props.src} />
-                <p class="img__description">{props.label}</p>
+    return (
+        <>            
+            <div class="project-details" >
+                <a href={path} className='project-link'>
+                    <img class="project-image" src={src} alt={alt} />
+                    
+                    <div class="project-description">
+                        <h5>{title}</h5>
+                        <p>{description}</p>
+                        {label} 
+                    </div>
+                    
                 </a>  
-                    <h5>{props.title}</h5>                        
-                    <p>{props.tagline}</p>
-                    <p>{props.description}</p>
-                </figure>
-                </div>
-        </div>
+            </div>
+        </>
     )
 }
 
