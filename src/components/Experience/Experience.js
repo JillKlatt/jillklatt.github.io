1ee67f6f5437fe911e8e0344848de2a6a0a397fe
JillKlatt <klatt.jill@gmail.com>
2021-07-19 22:12:29 -0400

    Add Experience

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -14,12 +14,12 @@ CustomTab.tabsRole = 'Tab';
 
 function Experience() {
 
-
+    const bullet = <span style={{ color: '#b07156' }}>&gt;  </span> 
 
     return (
         <div id='experience'>
             <div className='experience-title'>
-                <h3><span style={{ color: '#b07156' }}>&gt;</span> WORK EXPERIENCE</h3>
+                <h3>{bullet} WORK EXPERIENCE</h3>
 
             </div>
             <div >
@@ -33,22 +33,30 @@ function Experience() {
                     <TabPanel>
                         <h2>Assistant Manager - Mayfield Heights, OH</h2>
                         <ul className='job-description'>
-                            <li><span style={{ color: '#b07156' }}>&gt;</span> Handled customer relations, complaints, and assistance.</li>
-                            <li><span style={{ color: '#b07156' }}>&gt;</span> Oversaw daily operations, hiring and training staff, collaborating with vendors.</li>
+                            <li>{bullet} Handled customer relations, complaints, and assistance.</li>
+                            <li>{bullet} Developed and executed new training program for sales associates.</li>
+                            <li>{bullet} Completed computerized daily inventory reports with exceptional attention to detail.</li>
+                            <li>{bullet} Collaborated with vendors to ensure quality assurance and customer satisfaction.</li>
+                            <li>{bullet} Oversaw warehouse inventory, including executing purchase orders and coordinating deliveries to other store locations.</li>
                         </ul>
                     </TabPanel>
                     <TabPanel>
                         <h2>Kitchen Manager - Lakewood, OH</h2>
                         <ul className='job-description'>
-                            <li><span style={{ color: '#b07156' }}>&gt;</span> Coordinated and supervised staff according to food safety standards.</li>
-                            <li><span style={{ color: '#b07156' }}>&gt;</span> Developed menu, including food cost analysis.</li>
+                            <li>{bullet} Coordinated and supervised staff according to food safety standards.</li>
+                            <li>{bullet} Developed menu, including food cost analysis.</li>
+                            <li>{bullet} Orchestrated stock, quality, and waste management systems of food inventory.</li>
+                            <li>{bullet} Worked directly with suppliers on product releases and promotional events.</li>
+
                         </ul>
                     </TabPanel>
                     <TabPanel>
                         <h2>Bar Manager - Columbus, OH</h2>
                         <ul className='job-description'>
-                            <li><span style={{ color: '#b07156' }}>&gt;</span> Controlled the front of house image while ensuring customer satisfaction</li>
-                            <li><span style={{ color: '#b07156' }}>&gt;</span> Managed staff scheduling, training, and organized liquor orders and invoicing.</li>
+                            <li>{bullet} Controlled the front of house image while ensuring customer satisfaction.</li>
+                            <li>{bullet} Resolved customer grievances and complaints.</li>
+                            <li>{bullet} Managed staff scheduling, training, and conflict resolution.</li>
+                            <li>{bullet} Handled liquor inventory, ordering, and invoicing for bar supplies.</li>
                         </ul>
                     </TabPanel>
                 </Tabs>
