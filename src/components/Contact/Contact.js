e06a6860e1cf3425b373bf105b2a0a0d2cbfc99f
JillKlatt <klatt.jill@gmail.com>
2021-07-19 21:10:03 -0400

    Move buttons around and add form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,5 @@
 import './Contact.css'
+import ContactForm from './ContactForm'
 
 function Contact() {
 
@@ -8,28 +9,29 @@ function Contact() {
                 <h3><span style={{ color: '#b07156' }}>&gt;</span> CONTACT ME</h3>
             </div>
             <div className='contact-content'>
-                <div className='email'>
-                    <a class='button' href='mailto:klatt.jill@gmail.com'>klatt.jill@gmail.com</a>
-                </div>
                 <div className='socials'>
                     <ul>
-                    <li>
-                        <a href='https://www.linkedin.com/in/jill-klatt-39b2a9200/' >
-                            <img src='/linkinlogo.png' alt='linkedinlogo'/>
-                        </a>
-                    </li>
-                    <li>
-                        <a href='https://github.com/JillKlatt'>
-                            <img src='/githublogo.png' alt='github logo'/>
-                        </a>
-                    </li>
-                    <li>
-                        <a href='https://dev.to/jillklatt'>
-                            <img src='/devlogo.png' alt='dev-blog logo'/>
-                        </a>
-                    </li>
+                        <li>
+                            <a id='contact-email' href='mailto:klatt.jill@gmail.com'>klatt.jill@gmail.com</a>
+                        </li>
+                        <li>
+                            <a href='https://www.linkedin.com/in/jill-klatt-39b2a9200/' >
+                                <img src='/linkinlogo.png' alt='linkedinlogo' />
+                            </a>
+                        </li>
+                        <li>
+                            <a href='https://github.com/JillKlatt'>
+                                <img src='/githublogo.png' alt='github logo' />
+                            </a>
+                        </li>
+                        <li>
+                            <a href='https://dev.to/jillklatt'>
+                                <img src='/devlogo.png' alt='dev-blog logo' />
+                            </a>
+                        </li>
                     </ul>
                 </div>
+                <ContactForm />
             </div>
         </div>
     )
