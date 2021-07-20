af61be0287f31f6b1a4e85505ed33dc72cff389f
JillKlatt <klatt.jill@gmail.com>
2021-07-19 21:08:34 -0400

    Add ids to intro button

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -15,8 +15,8 @@ function Intro() {
         < p>Experienced Ruby on Rails and React JS developer with a background in the service industry. <br></br>People-oriented individual with a positive attitude and strong interpersonal skills. </p>
         </div>
         <div className='intro contact'>
-          <a class='button' href='mailto:klatt.jill@gmail.com'>Send an Email</a>
-          <Link class='button' smooth to='#contact'>More Contact Options</Link>
+          <a id='intro-button' class='button' href='mailto:klatt.jill@gmail.com'>Send an Email</a>
+          <Link id='intro-button' class='button' smooth to='#contact'>More Contact Options</Link>
         </div>
       </div>
     )
