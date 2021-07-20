9d48950639b9f694672d6c758370700cc10139ec
JillKlatt <klatt.jill@gmail.com>
2021-07-20 01:29:20 -0400

    Add sticky email

--- src/components/Email/Email.js
+++ src/components/Email/Email.js
@@ -2,9 +2,17 @@ import './Email.css'
 
 function Email() {
 
+    // const displayEmail = (e) => {
+    //     e.target.style.background = '#B07156';
+    //     return (
+    //         <div>
+    //             <h3>klatt.jill@gmail.com</h3>
+    //         </div>
+    //     )
+    // }onMouseEnter={displayEmail}
     return (
         <a id='sticky-email' href='mailto:klatt.jill@gmail.com'>
-        <img src='/temail4.jpg' alt='gmail logo' />
+        <img src='/temail4.jpg' alt='gmail logo'  />
     </a>
     )
 
