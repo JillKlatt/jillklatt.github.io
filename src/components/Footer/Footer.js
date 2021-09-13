04d290d1813ca0bfec922ad4184801d9a11b5abb
JillKlatt <klatt.jill@gmail.com>
2021-09-13 16:03:13 -0400

    Edit footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,6 @@ function Footer() {
     return (
         <div id='footer'>
             <p id='jill'>Created by Jill Klatt 2021</p>
-            <p id='brittany'>Inspired by <a href='https://github.com/bchiang7/v4'>Brittany Chiang</a></p>
         </div>
     )
 
